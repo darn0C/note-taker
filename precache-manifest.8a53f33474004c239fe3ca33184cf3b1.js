658223d5a4624d2825593ab852b5820fd5ee5ed3
darn0C <conradremon7@gmail.com>
2020-06-19 01:22:55 -0300

    Updates

---
+++ precache-manifest.8a53f33474004c239fe3ca33184cf3b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a6dc98abd3134254063a242c46eb0f",
+    "url": "/note-taker/index.html"
+  },
+  {
+    "revision": "7919d117d29fdeb0fe15",
+    "url": "/note-taker/static/js/2.6b070099.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/note-taker/static/js/2.6b070099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15048b8adde77138d8d3",
+    "url": "/note-taker/static/js/main.6645a69f.chunk.js"
+  },
+  {
+    "revision": "06aaa25b045d5911c9b2",
+    "url": "/note-taker/static/js/runtime-main.c97a50f6.js"
+  }
+]);
